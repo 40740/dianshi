3bb6aab7471f01a1e8d43bf5883a7de3e790da72
老耗 <39937268+40740@users.noreply.github.com>
2022-03-29 11:56:29 +0800

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,3 +1,7 @@
+<?php
+
+//https://vod.stream.hndt.com/HeNanFF3/20220319/HeNanFF3_20220319_0000_0240.mp4
+
 $today = date("Ymd");
 
 $url = "https://vod.stream.hndt.com/HeNanFF3/".$today."/HeNanFF3_".$today."_0000_0240.mp4";
